08aaa8a3e8d2f1cb6d8a10378f602305cd8f5958
AhmedNabil <ahmednabil.gad18@gmail.com>
2021-07-17 18:08:34 +0200

    Dio Module Update V1.1

--- Dio Module/Dio_Cfg.c
+++ Dio Module/Dio_Cfg.c
@@ -8,6 +8,8 @@
 #include "Dio_Cfg.h"
 #include "Dio.h"
 
+
+/* Array of structures, each struct holds configuration for given PIN */
 strDio_Config_t strDio_pins[DIO_USED_PINS_NUM]=
 {
 	{DioConf_LED1_PORT_NUM, DioConf_LED1_PIN_NUM, DIO_PIN_DIR_OUTPUT, PIN_LOW},
@@ -21,12 +23,3 @@ strDio_Config_t strDio_pins[DIO_USED_PINS_NUM]=
 	{DioConf_IN4_PORT_NUM, DioConf_IN4_PIN_NUM, DIO_PIN_DIR_OUTPUT, PIN_LOW},
 	{DioConf_PWM_PORT_NUM, DioConf_PWM_PIN_NUM, DIO_PIN_DIR_OUTPUT, PIN_LOW}
 };
-
-/*
-typedef struct
-{
-	uint8_t u8_PortNum;
-	uint8_t u8_PinNum;
-	enuDio_Direction_t u8_Direction;
-	uint8_t u8_InitState; 
-}strDio_Config_t;*/
